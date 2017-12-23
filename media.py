c17cd27b17580602c97698e54dbb1cc3c2929d00
Salma Mohammed <sm.71175@gmail.com>
2017-12-23 21:47:39 +0200

    create media class for all movies instances

--- media.py
+++ media.py
@@ -1,12 +1,27 @@
 """ Define the media class which have the media information"""
 
-class Media(object):
-	def __init__(self, movieTitle, storyLine, posterImage, trailerYoutube):
-		self.title = movieTitle
-		self.story = storyLine
-		self.poster_image_url = posterImage
-		self.trailer_youtube_url = trailerYoutube
+import webbrowser
 
-	def show_trailer(self):
-		""" Open movie trailer in browser. """
-		webbrowser.open(self.trailer_youtube_url)
+
+class Media(object):
+
+    def __init__(
+        self,
+        movieTitle,
+        storyLine,
+        posterImage,
+        trailerYoutube,
+        ):
+
+        self.title = movieTitle
+        self.story = storyLine
+        self.poster_image_url = posterImage
+        self.trailer_youtube_url = trailerYoutube
+
+    def show_trailer(self):
+        """ Open movie trailer in browser. """
+
+        webbrowser.open(self.trailer_youtube_url)
+
+
+
